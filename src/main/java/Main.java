5e95735c575dc3f1c923de7ae02fed9c50326596
AlexBobryshev <darknessrider1@rambler.ru>
2019-06-11 23:09:51 +0300

    5th commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,20 @@
 import models.Club;
 import models.Player;
 import services.ClubService;
+import services.PlayerService;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         ClubService clubService = new ClubService();
+        PlayerService playerService = new PlayerService();
 
         Club club = new Club("Rapid Fire");
         clubService.saveClub(club);
         Player player = new Player("Prokopenko", 28);
         club.addPlayer(player); //should be also saved using PlayerService?
+        playerService.savePlayer(player);
         //System.out.println(player);
         clubService.updateClub(club);
         //System.out.println(club);
